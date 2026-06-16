e47d9ea05c7c8c763f968f246690aeef520ed307
Teacher Egg <teacherEgg@teacheregg.com>
2026-06-16 23:58:24 +0800

    项目创建

---
+++ src/main/java/com/czltnb/zhi_xiang_backend/ZhiXiangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.czltnb.zhi_xiang_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiXiangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiXiangBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czltnb/zhi_xiang_backend/ZhiXiangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czltnb.zhi_xiang_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhiXiangBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
